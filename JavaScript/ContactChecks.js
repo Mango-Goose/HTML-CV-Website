0867a68c7f5dc9dcf303938efb40b909b5fae1b7
Mango-Goose <113191075+Mango-Goose@users.noreply.github.com>
2024-02-19 15:27:29 +0000

    Adding some JavaScript validation and adding more content
    Yeah just adding more of the requirements to the website and trying to work as fast as possible to meet the deadline and not have to rely on my extenuating circumstances for the assignment

--- JavaScript/ContactChecks.js
+++ JavaScript/ContactChecks.js
@@ -1,30 +1,45 @@
 // JavaScript source code
-window.onload = function () {
-    let contactForm = document.getElementByID("Contact-Form");
-    //let firstEmail = document.getElementById("email");
-    //let confirmEmail = document.getElementById("confirmEmail");
-}
-let validEmail = false;
+let contactForm = document.getElementById("Contact-Form");
+let firstEmail = contactForm.email;
+let confirmEmail2 = contactForm.confirmEmail;
+const errorText = document.getElementById("error");
+
+let validEmail = false; 
 firstEmail.onchange = checkEmails;
+confirmEmail2.onchange = checkEmails;
+
+/*let signup_button = document.getElementById("submit");
+signup_button.onclick = checkAll;
+
+function checkAll() {
+    checkEmails();
+    //checkDate();
+    //validateForm();
+}
 
 validateForm = function () {
 
 }
-
+*/
 function checkEmails() {
     let firstEmail = contactForm.email;
-    let confirmEmail2 = contactForm.confirmEmail;
-    //have to make sure that the first email is of the right format first
+    let confirmEmail2 = document.getElementById("confirmEmail");
 
-    if (firstEmail == confirmEmail) {
+    //have to make sure that the first email is of the right format first
+    if (firstEmail .@.) {
+
+    }
+    if (firstEmail === confirmEmail2) {
         validEmail = true;
-        text = "yes";
+        console.log("YES");
+        errorText.innerText = "yes";
     }
     else {
-        text="no"
+        errorText.innerText = "Emails do not match";
+        console.log("EMAILS DONT MATCH");
         validEmail = false;
     }
-    document.getElementById("test") = text;
+    //document.getElementById("test") = text;
 }
 
 function checkDate () {
